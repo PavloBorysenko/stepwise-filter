d7184761bc6f06463a244e6df9d97f0da9c77cbb
PavloBorysenko <pablodevelophelp@gmail.com>
2025-01-08 18:45:25 +0100

    init phpunit

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '65a17f89a830e4d23154');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a2e9573574ec52dc892a');
---
+++ tests/testAjax.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Test_Ajax
+ *
+ * @package Husky_Block
+ */
+
+
+/**
+ * Test_Ajax.
+ */
+class testAjax extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}

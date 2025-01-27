5be01d30067f53b9c6efc0c9b67da9592e66f99c
PavloBorysenko <pablodevelophelp@gmail.com>
2025-01-27 18:03:14 +0100

    init lints

--- stepwise-filter.php
+++ stepwise-filter.php
@@ -17,6 +17,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+require_once __DIR__ . '/vendor/autoload.php';
+
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
  * Behind the scenes, it registers also all assets so they can be enqueued
